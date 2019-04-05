8900ff73dd18c6aac567598ccad883c9b6ca22e9
Adrian Badarau <abadarau@lenovo.com>
2019-04-05 11:21:52 +0200

    finished the assigment

--- src/main/java/org/springframework/samples/petclinic/sfg/BaseConfig.kt
+++ src/main/java/org/springframework/samples/petclinic/sfg/BaseConfig.kt
@@ -1,7 +1,9 @@
 package org.springframework.samples.petclinic.sfg
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 open class BaseConfig {
@@ -10,8 +12,19 @@ open class BaseConfig {
         return HearingInterpreter(wordProducer)
     }
 
+    @Bean(name = ["yannyInterpreter"])
+    open fun yannyInterpreter(yannyWordProducer: YannyWordProducer): HearingInterpreter {
+        return HearingInterpreter(yannyWordProducer)
+    }
+
     @Bean
+    @Primary
     open fun laurelWordProducer(): LaurelWordProducer {
         return LaurelWordProducer()
+    }
+
+    @Bean
+    open fun yannyWordProducer(): YannyWordProducer {
+        return YannyWordProducer()
     }
 }
--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.kt
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.kt
@@ -5,6 +5,7 @@ import org.junit.Test
 import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -15,9 +16,19 @@ class HearingInterpreterTest {
     @Autowired
     lateinit var hearingInterpreter: HearingInterpreter
 
+    @Autowired
+    @Qualifier("yannyInterpreter")
+    lateinit var yannyHearingInterpreter: HearingInterpreter
+
     @Test
     fun whatIheard() {
         val whatIheard = hearingInterpreter.whatIheard()
         assertEquals("Laurel", whatIheard)
+    }
+
+    @Test
+    fun testThatYannyInterpreterWorks(){
+        val whatIheard = yannyHearingInterpreter.whatIheard()
+        assertEquals("Yanny", whatIheard)
     }
 }
