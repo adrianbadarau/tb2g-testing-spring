f698b970cd0a9b7ef25fef49e4951d52089de875
Adrian Badarau <abadarau@lenovo.com>
2019-04-05 14:18:16 +0200

    added spring controller test using mocMVC

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.kt
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.kt
@@ -2,8 +2,6 @@ package org.springframework.samples.petclinic.web
 
 import com.nhaarman.mockitokotlin2.*
 import org.assertj.core.api.Assertions
-import org.junit.Assert
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -13,10 +11,11 @@ import org.mockito.Spy
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.samples.petclinic.model.Vet
 import org.springframework.samples.petclinic.model.Vets
-import org.springframework.samples.petclinic.service.ClinicService
 import org.springframework.samples.petclinic.service.ClinicServiceImpl
-import org.springframework.ui.Model
-import kotlin.random.Random
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 @ExtendWith(MockitoExtension::class)
 class VetControllerTest {
@@ -24,10 +23,12 @@ class VetControllerTest {
     @Mock
     lateinit var clinicService: ClinicServiceImpl
     @Spy
-    lateinit var model: MutableMap<String, Any>
+    lateinit var testModel: MutableMap<String, Any>
 
     @InjectMocks
     lateinit var vetController: VetController
+
+    lateinit var mockMvc: MockMvc
 
     @BeforeEach
     fun setUp() {
@@ -42,16 +43,18 @@ class VetControllerTest {
             }
             return@willReturn vets
         }
+
+        mockMvc = MockMvcBuilders.standaloneSetup(vetController).build()
     }
 
     @Test
     fun testShowVetList() {
         //when
-        val resp = vetController.showVetList(model)
+        val resp = vetController.showVetList(testModel)
         //then
         Assertions.assertThat(resp).isEqualTo("vets/vetList")
         then(clinicService).should().findVets()
-        then(model).should().put(eq("vets"), any<Vets>())
+        then(testModel).should().put(eq("vets"), any<Vets>())
     }
 
     @Test
@@ -63,5 +66,13 @@ class VetControllerTest {
         Assertions.assertThat(resp).isNotNull
         Assertions.assertThat(resp.vetList.size).isEqualTo(5)
 
+    }
+
+    @Test
+    internal fun testControllerShowVetList() {
+        mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk)
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList"))
     }
 }
